9e89022bc8f2749fbd3954e2ce6f9ecb65fe8035
KonkenBonken <konrad05pettersson@gmail.com>
2022-11-14 22:14:21 +0100

    Added memoization
    Making execution over 2000 times faster

--- src/fibonacci/fibonacci.java
+++ src/fibonacci/fibonacci.java
@@ -4,17 +4,24 @@ import java.util.HashMap;
 
 public class Fibonacci {
 
+  static HashMap<Integer, Integer> memoization = new HashMap<Integer, Integer>();
+
   public static void main(String[] args) {
-
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 47; i++)
       System.out.println(fibonacci(i));
-
   }
 
   private static int fibonacci(int n) {
+    if (memoization.containsKey(n))
+      return memoization.get(n);
+
     if (n <= 1)
       return n;
-    return fibonacci(n - 2) + fibonacci(n - 1);
+
+    int value = fibonacci(n - 2) + fibonacci(n - 1);
+    memoization.put(n, value);
+
+    return value;
   }
 
 }
